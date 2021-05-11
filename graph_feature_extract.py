905174ffe70a035a4ecc93633b4761266e9e5260
Wanncye <535593370@qq.com>
2021-05-11 12:24:33 +0800

    use GAT 5 mask feature fc_num=128

--- graph_feature_extract.py
+++ graph_feature_extract.py
@@ -45,7 +45,7 @@ glcm_train_feature = torch.load('./data/mask_feature/glcm_train_feature.pt')
 glcm_test_feature = torch.load('./data/mask_feature/glcm_test_feature.pt')
 train_label = torch.load('./data/mask_feature/train_label.pt')
 test_label = torch.load('./data/mask_feature/test_label.pt')
-adj = Variable(torch.ones((4, 4)))
+adj = Variable(torch.ones((5, 5)))
 
 best_test_acc = 0
 for epoch in range(100):
@@ -58,7 +58,8 @@ for epoch in range(100):
     for index, one_nodule_feature in enumerate(zip(googlenet_train_feature, 
                                                     resnet_train_feature, 
                                                     vgg_train_feature,
-                                                    hog_train_feature)):
+                                                    hog_train_feature,
+                                                    lbp_train_feature)):
         temp = torch.zeros((len(one_nodule_feature),512))
         for i, feature in enumerate(one_nodule_feature):
             temp[i] = feature
@@ -84,7 +85,8 @@ for epoch in range(100):
     for index, one_nodule_feature in enumerate(zip(googlenet_test_feature, 
                                                     resnet_test_feature, 
                                                     vgg_test_feature,
-                                                    hog_test_feature)):
+                                                    hog_test_feature,
+                                                    lbp_test_feature)):
         temp = torch.zeros((len(one_nodule_feature),512))
         for i, feature in enumerate(one_nodule_feature):
             temp[i] = feature
