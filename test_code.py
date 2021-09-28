4e185e75d6c7d8ab34e40704945e89a265ade6cf
Wanncye <535593370@qq.com>
2021-09-28 15:56:07 +0800

    计算准确率的均值方差并切换分支

--- test_code.py
+++ test_code.py
@@ -10,16 +10,7 @@ import matplotlib.pyplot as plt
 
 from utils import get_dataset_label_pt
 
-prob = np.array([
-    [1,5],
-    [5,8],
-    [8,6],
-    [8,6],
-    [6,4],
-    [5,7]
-])
-
-a = prob[:,1].repeat(2, axis=0).reshape(-1,2) * [0.1,0.2]
-print(a)
-
+a = [0.955,0.913,0.899,0.874,0.925]
+print(np.mean(a))
+print(np.std(a))
 
